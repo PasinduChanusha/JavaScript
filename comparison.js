6ef2a716e2d76d80d2648e3d55b160343e7a11b6
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-06-17 01:54:27 +0530

    ternery

---
+++ comparison.js
@@ -0,0 +1,14 @@
+/* 
+(==) -> equal 
+(===) -> equal value or equal type
+(!==) -> not equal value or not equal type
+(>),(<),(>=),(=<),(!=)
+
+*/
+
+// Ternery operator
+
+var x;
+x = prompt("Enter number: ");
+
+x > 5 ? console.log("Grater") : console.log("Less");
