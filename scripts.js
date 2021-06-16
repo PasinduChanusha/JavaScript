102c9272fa4adb03a08141621cb2fb5f8eed6b7d
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-06-16 23:34:01 +0530

    init

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
 // external script file
 
-var firstName = 'Pasindu';
+var firstName = "Pasindu";
+var lastName = "Chanusha";
+var age = 24;
 
-console.log(firstName);
+console.log("My name is" + firstName);
+console.log(lastName);
+console.log("I am " + age + "years old");
+
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(age));
