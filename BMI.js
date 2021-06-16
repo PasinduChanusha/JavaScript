5e26323f5f3abe8cdf56582fcbc359c505e33057
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-06-17 01:21:26 +0530

    BMI

---
+++ BMI.js
@@ -0,0 +1,7 @@
+var bmi, height, weight;
+height = prompt("Enter Your Height from Miters: ");
+weight = prompt("Enter Your Weight From kg s: ");
+
+bmi = weight / (height ** 2);
+
+console.log("Your BMI is: " + bmi);
