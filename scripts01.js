b3a2c1404e8ccd89253c221ae104fdda2df9e7a9
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-06-17 00:48:31 +0530

    cal

---
+++ scripts01.js
@@ -0,0 +1,19 @@
+var x, y, z;
+x = prompt("X: ");
+y = prompt("Y: ");
+
+// add
+z = x + y;
+console.log(z);
+
+// sub
+z = x - y;
+console.log(z);
+
+// mul
+z = x * y;
+console.log(z);
+
+// div
+z = x / y;
+console.log(z);
