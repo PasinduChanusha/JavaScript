53b7fd05415b62c6245d4c0845b7f3714a1c6f3d
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-06-17 00:36:26 +0530

    input

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
 // external script file
 
+// variable declare
 var firstName = "Pasindu";
 var lastName = "Chanusha";
 var age = 24;
 
+// print
 console.log("My name is" + firstName);
 console.log(lastName);
 console.log("I am " + age + "years old");
 
+// find data type
 console.log(typeof(firstName));
 console.log(typeof(lastName));
 console.log(typeof(age));
+
+// make alert
+alert("My name is " + firstName + ".");
+
+// 
+var height;
+height = prompt("What is your height: ");
+alert("My height is: " + height);
